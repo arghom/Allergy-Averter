f4b8d45945ffae2248cb40b47338247f9f4b92a5
arghom <argho9703@gmail.com>
2024-08-05 03:47:18 -0400

    Added Recipes page and added link functionality

--- allergy-averter/src/Recipes.jsx
+++ allergy-averter/src/Recipes.jsx
@@ -1,10 +1,12 @@
 import { SessionContext } from './SessionProvider'
 import { useContext,useEffect,useState } from 'react'
 import supabase from './utils/supabase'
+import { useNavigate } from 'react-router-dom'
 function Recipes() {
     //verifies login
     const session = useContext(SessionContext)
     const [recipes,setRecipes] = useState([])
+    const navigate = useNavigate()
     useEffect(() => {
         const getRecipes = async () => {
             if(session){
@@ -19,7 +21,11 @@ function Recipes() {
         }
         getRecipes()
     },[session])
+    console.log(recipes)
 
+    const navigateToDisplayRecipe = ( {selectedRecipe} ) => {
+        navigate('/displayRecipe',{state: {recipe: selectedRecipe}})
+    }
   return (
     <>
     <div className='flex justify-center items-center flex-col'>
@@ -29,11 +35,15 @@ function Recipes() {
             <tbody>
             <tr>
                 <th>Recipe Name</th>
-                <th>Ingredients</th>
                 <th>Free From</th>
                 <th>Link To</th>
             </tr>
             </tbody>
+            {recipes ? <tbody>
+                {recipes.map((recipe,index) => {
+                    return <tr key={index}><td>{recipe.recipe.Recipe.Name}</td><td>{recipe && recipe.recipe.Recipe.Allergens.join(', ')}</td><td><button className='text-blue-600' onClick={() => navigateToDisplayRecipe({selectedRecipe : recipe.recipe.Recipe})}>Open Recipe</button></td></tr>
+                })}
+            </tbody> : <div></div>}
         </table>
     </div>
     </div>
